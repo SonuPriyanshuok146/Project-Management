4aef7066a19ce704a37aa432068ee1154720fef8
SonuPriyanshuok146 <priyanshuok146@yahoo.com>
2026-03-22 20:18:22 +0530

    Adding project routes

--- src/app.js
+++ src/app.js
@@ -25,10 +25,14 @@ app.use(cors({
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 import authRouter from "./routes/auth.routes.js"
+import projectRouter from "./routes/project.routes.js";
+
 
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/projects", projectRouter);
+
 
 
 
